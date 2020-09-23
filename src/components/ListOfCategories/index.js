a56ca7326f2b7a2b26adba00241cdb99aa78e91c
Rosa <rosaislp@gmail.com>
2020-09-23 10:03:29 -0500

    creating a custom hook to data fetching

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,20 +2,52 @@ import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+// Esta será la responsable de hacer el fetchin de datos
+const useCategoriesData = () => {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     window.fetch('https://petgram-server-rosaisela31.vercel.app/categories')
       .then(res => res.json())
-      .then(response => { setCategories(response) })
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
   }, [])
 
-  return (
-    <List>
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    // Limpiando el efecto
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        loading ? <Item key='loading'><Category /></Item> : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
